ae515edafa10e993fdf6006758c910bffde2f2a3
22550320-MSDP <124701976+22550320-MSDP@users.noreply.github.com>
2023-03-26 20:53:55 -0600

    EVA2_13_FIGURAS_ABSTRACTAS

---
+++ EVA2_13_FIGURAS_ABSTRACTAS/src/eva2_13_figuras_abstractas/EVA2_13_FIGURAS_ABSTRACTAS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_13_figuras_abstractas;
+
+/**
+ * @author Danna Paola Maldonado Sepúlveda
+ * 22550320 grubo b
+ */
+public class EVA2_13_FIGURAS_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //figura f = new figura(); no se pueden crear objetos de la claase figura
+        circulo c = new circulo();
+    }
+    
+}
