4d150a43c2c308c8e081e2e488126674e385d1de
22550320-MSDP <124701976+22550320-MSDP@users.noreply.github.com>
2023-03-26 20:53:19 -0600

    EVA2_12_CLASES_ABSTRACTAS

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author Danna Paola Maldonado Sepúlveda
+ * 22550320 grubo b
+ */
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        prueba prueba = new prueba();
+        // no se puee crear objetos(instancias) de clases abstractas
+        //prueba_abstracta pa = new prueba_abstracta();
+    }
+}
+
+class prueba {
+
+}
+abstract class prueba_abstracta{}
