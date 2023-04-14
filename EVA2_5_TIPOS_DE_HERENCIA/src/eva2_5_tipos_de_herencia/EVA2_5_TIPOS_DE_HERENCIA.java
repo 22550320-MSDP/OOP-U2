0f2b16b6f095a6afb585654cace0854ca4901af4
22550320-MSDP <124701976+22550320-MSDP@users.noreply.github.com>
2023-04-14 11:56:09 -0600

    EVA2_5_TIPOS_DE_HERENCIA

---
+++ EVA2_5_TIPOS_DE_HERENCIA/src/eva2_5_tipos_de_herencia/EVA2_5_TIPOS_DE_HERENCIA.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_5_tipos_de_herencia;
+
+/**
+ *
+ * @author Usuario Final
+ */
+public class EVA2_5_TIPOS_DE_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         }
+        // Existen 2 tipos de herencia: múltiple y simple     
+        // Java solo permite la simple     
+    }
+        class SuperClase{ 
+        
+    }
+
+    class OtraSuperClase{
+   //No se hereda en mas de 2 superclases   
+        
+    }
+
+    class BaseClase extends SuperClase{
+    //Funciona solo para una clase
+        
+    }
